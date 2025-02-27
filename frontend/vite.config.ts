a26851ae2d1c75694db9ffba3ec02df0f3d107e3
IsacEP <Isac.eklind.pettersson@outlook.com>
2025-02-27 21:50:22 +0100

    Moved docs/ to the root of the project

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "/ITM8/",
   build: {
-    outDir: "docs",
+    outDir: "../docs",
   },
   plugins: [react()],
   server: {
